bde839be9c099628f71e8a0979a2f2e406a27b2e
MadhanMohanReddy <mreddy@hortonworks.com>
2016-10-07 15:16:41 +0530

    Adding font awesome css

--- contrib/views/beacon/src/main/resources/ui/ember-cli-build.js
+++ contrib/views/beacon/src/main/resources/ui/ember-cli-build.js
@@ -22,6 +22,26 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+  
+  app.import('bower_components/font-awesome/css/font-awesome.min.css');
+  app.import('bower_components/font-awesome/fonts/FontAwesome.otf', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff', {
+    destDir: 'fonts'
+  });
+  app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff2', {
+    destDir: 'fonts'
+  });
 
   return app.toTree();
 };
